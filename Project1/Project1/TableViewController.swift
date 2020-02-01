ccab7c9772e6efc4df6231ddfa50b8938e945861
hyunsik won <shik0601@gmail.com>
2020-02-01 18:52:15 +0900

    first commit

---
+++ Project1/Project1/TableViewController.swift
@@ -0,0 +1,59 @@
+//
+//  TableViewController.swift
+//  Project1
+//
+//  Created by 원현식 on 2020/02/01.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var pictures: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        
+        let path = Bundle.main.resourcePath!
+        ///Users/hyunsikwon/Library/Developer/CoreSimulator/Devices/A918701B-6D05-4056-8306-F393D2376FB7/data/Containers/Bundle/Application/7B226FE4-4FA4-499F-8F22-1DDF6415A790/Project1.app
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+                
+            }
+        }
+        
+        print(pictures)
+        
+        
+    }
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return pictures.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        
+        cell.textLabel?.text = pictures[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    
+}
