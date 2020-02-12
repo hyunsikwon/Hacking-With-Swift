83eae70bf19fd762a3faffcb33b4419de632a864
hyunsik won <shik0601@gmail.com>
2020-02-12 21:16:23 +0900

    Project5 - 1 완료... 값 캡쳐, ARC 이해하기

---
+++ Project5/Project5/TableViewController.swift
@@ -0,0 +1,82 @@
+//
+//  TableViewController.swift
+//  Project5
+//
+//  Created by 원현식 on 2020/02/12.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+
+        cell.textLabel?.text = usedWords[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    //MARK: - Methods
+    
+    private func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll()
+//        calling reloadData() forces it to call numberOfRowsInSection again, as well as calling cellForRowAt repeatedly.
+        tableView.reloadData()
+    }
+    
+    @objc private func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+        
+    }
+    
+    private func submit(_ answer: String) {
+        
+    }
+
+
+}
