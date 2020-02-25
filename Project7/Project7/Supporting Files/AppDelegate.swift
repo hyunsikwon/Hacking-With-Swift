71bed2d4a105b76f4f365400a2a03073c1515ac9
hyunsik won <shik0601@gmail.com>
2020-02-25 22:59:04 +0900

    Challenge 끝

--- Project7/Project7/Supporting Files/AppDelegate.swift
+++ Project7/Project7/Supporting Files/AppDelegate.swift
@@ -11,17 +11,17 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    var window: UIWindow?
+//    var window: UIWindow?
     
     // This gets called by iOS when the app has finished loading and is ready to be used,
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
-        if let tabBarController = window?.rootViewController as? UITabBarController {
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let vc = storyboard.instantiateViewController(withIdentifier: "NavController")
-            vc.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 1)
-            tabBarController.viewControllers?.append(vc)
-        }
+//        if let tabBarController = window?.rootViewController as? UITabBarController {
+//            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//            let vc = storyboard.instantiateViewController(withIdentifier: "NavController")
+//            vc.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 1)
+//            tabBarController.viewControllers?.append(vc)
+//        }
         
         return true
     }
--- Project7/Project7/TableViewController.swift
+++ Project7/Project7/TableViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class TableViewController: UITableViewController {
     
     var petitions = [Petition]()
-
+    var filteredPetitions = [Petition]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,7 +23,12 @@ class TableViewController: UITableViewController {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
 
         }
-    
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredit))
+        let filterBarButton = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterTapped))
+        let showAllBarButton = UIBarButtonItem(title: "Show All", style: .plain, target: self, action: #selector(showAll))
+        
+        navigationItem.leftBarButtonItems = [filterBarButton,showAllBarButton]
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
@@ -37,15 +43,15 @@ class TableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return petitions.count
+        return filteredPetitions.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        cell.textLabel?.text = petitions[indexPath.row].title
-        cell.detailTextLabel?.text = petitions[indexPath.row].body
+        cell.textLabel?.text = filteredPetitions[indexPath.row].title
+        cell.detailTextLabel?.text = filteredPetitions[indexPath.row].body
 
         return cell
     }
@@ -63,6 +69,7 @@ class TableViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            filteredPetitions = petitions
             tableView.reloadData()
         }
     }
@@ -71,6 +78,49 @@ class TableViewController: UITableViewController {
         let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
+    }
+    
+    @objc private func showCredit() {
+        let ac = UIAlertController(title: "Credits", message: "We The People API of the Whitehouse.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        
+        ac.addAction(action)
+        present(ac,animated: true)
+        
+    }
+    
+    @objc private func filterTapped() {
+        let ac = UIAlertController(title: nil, message: "검색", preferredStyle: .alert)
+        ac.addTextField()
+        
+        let action = UIAlertAction(title: "search", style: .default, handler: { [weak self, weak ac]_ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            if !answer.isEmpty{
+                self?.showFilteredList(answer)
+            }
+            
+        })
+        
+        ac.addAction(action)
+        
+        present(ac,animated: true)
+    }
+    
+    @objc private func showAll() {
+        filteredPetitions = petitions
+        tableView.reloadData()
+    }
+    
+    private func showFilteredList(_ answer: String) {
+        filteredPetitions = []
+        
+        for petition in petitions {
+            if petition.title.contains(answer) {
+                filteredPetitions.append(petition)
+            }
+        }
+        
+        tableView.reloadData()
     }
     
     
