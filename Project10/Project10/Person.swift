1ff3c095f73ec687e57d0b869f31627b4b112863
hyunsik won <shik0601@gmail.com>
2020-03-04 21:12:42 +0900

    Project10 Challenge까지 함

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by 원현식 on 2020/03/04.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  Project10
+//
+//  Created by 원현식 on 2020/03/04.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
