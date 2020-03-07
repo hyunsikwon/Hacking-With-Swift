3f83671c0e368c5ba3b097b1ae313bf666e6f150
hyunsik won <shik0601@gmail.com>
2020-03-07 20:54:35 +0900

    다음에 할 것

---
+++ Project11/Project11/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project11
+//
+//  Created by 원현식 on 2020/03/07.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
