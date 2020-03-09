0397d3f474f13f7f42fb160b4b849aac1fc993dc
hyunsik won <shik0601@gmail.com>
2020-03-09 22:57:09 +0900

    Project12b 완료

---
+++ Project12b/Project10/CollectionViewController.swift
@@ -0,0 +1,140 @@
+//
+//  CollectionViewController.swift
+//  Project10
+//
+//  Created by 원현식 on 2020/03/04.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var people = [Person]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        
+        let defaults = UserDefaults.standard
+        if let savedPeople = defaults.object(forKey: "people") as? Data {
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                people = try jsonDecoder.decode([Person].self, from: savedPeople)
+            } catch {
+                print("Failed to load people")
+                
+            }
+        }
+        
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        
+        let person = people[indexPath.item]
+        
+        cell.name.text = person.name
+        
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+        
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+        
+        let actionSheet = UIAlertController(title: "선택", message: nil, preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Rename", style: .default, handler: { _ in
+            let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+            ac.addTextField()
+            
+            ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            
+            ac.addAction(UIAlertAction(title: "Add", style: .default, handler: { [weak self, weak ac] _ in
+                guard let newName = ac?.textFields?[0].text else { return }
+                person.name = newName
+                self?.collectionView.reloadData()
+                self?.save()
+            }))
+            
+            self.present(ac, animated: true)
+        }))
+        
+        
+        actionSheet.addAction(UIAlertAction(title: "Delete", style: .default, handler: { [weak self]_ in
+            self?.people.remove(at: indexPath.item)
+            self?.collectionView.deleteItems(at: [indexPath])
+            self?.save()
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(actionSheet, animated: true)
+        
+    }
+    
+    
+    
+    
+    // MARK: - Private Methods
+    @objc private func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true // allows the user to crop the picture they select.
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    private func save() {
+        let encoder = JSONEncoder()
+        if let savedData = try? encoder.encode(people) {
+            let defualts = UserDefaults.standard
+            defualts.set(savedData, forKey: "people")
+        } else {
+            print("Failed to save people.")
+        }
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString // generates a Universally Unique Identifier and is perfect for a random filename.
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegImage = image.jpegData(compressionQuality: 0.8) {
+            try? jpegImage.write(to: imagePath)
+        }
+        
+        let person = Person(name: "Unknown", image: imageName)
+        people.append(person)
+        collectionView.reloadData()
+        
+        dismiss(animated: true)
+    }
+    
+    
+}
