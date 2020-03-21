732f41919732d69c59f038bebd7f4a2e9fc966a5
hyunsik won <shik0601@gmail.com>
2020-03-21 16:22:52 +0900

    project18 끝

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by 원현식 on 2020/03/21.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+
+
+    }
+
+
+}
+
